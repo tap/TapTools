f1372fe6a36f757126abf260f2c53b789b854aa8
+1m place <tim@74objects.com>
2012-10-20 15:25:55 -0700

    build.rb: now build ObjectiveMax prior to building any of TapTools.

--- build.rb
+++ build.rb
@@ -10,5 +10,18 @@ projectName = projectNameParts.last;
 projectName.gsub!(/Jamoma/, "")
 ENV['JAMOMAPROJECT'] = projectName
 
+@projectName = projectName
+@log_root = "./logs-#{@projectName}"
+@svn_root = "../#{@projectName}"
+@fail_array = Array.new
+@zerolink = false
+
 Dir.chdir "#{glibdir}/../Support"
+
+require "#{glibdir}/../Support/jamomalib.rb"
+
+create_logs(@projectName)
+zero_count
+build_project("#{glibdir}/../Support/objectivemax/MaxObject", "MaxObject.xcodeproj", "Deployment", true, nil, false)
+
 load "build.rb"
