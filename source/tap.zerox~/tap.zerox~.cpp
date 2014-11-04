0a1b0e7e0383b04c36f6c101c2ecc3b705be350e
timothy place <tim@74objects.com>
2014-11-04 12:47:41 -0600

    tap.zerox~: updated

--- source/tap.zerox~/tap.zerox~.cpp
+++ source/tap.zerox~/tap.zerox~.cpp
@@ -35,7 +35,7 @@ static t_class *zerox_class;					// Required. Global pointing to this class
 /************************************************************************************/
 // Main() Function
 
-extern "C" int TTCLASSWRAPPERMAX_EXPORT main(void)
+extern "C" int C74_EXPORT main(void)
 {
 	t_class *c;
 
