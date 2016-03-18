0214903ad0757527f8d16efcc5bf66efea1c73a2
Timothy Place <tim@cycling74.com>
2016-03-18 10:34:05 -0500

    initial appveyor files

---
+++ source/build.ps1
@@ -0,0 +1,14 @@
+$env:CMAKE = $env:CMAKE_PATH + "\bin\cmake.exe"
+if ( $env:PLATFORM -eq "x64" ) {
+  $env:CMAKE_GENERATOR = "Visual Studio 12 2013 Win64"
+  $env:CUSTOM_FLAG = "-DWIN64:Bool=True"
+} else {
+  $env:CMAKE_GENERATOR = "Visual Studio 12 2013"
+  $env:CUSTOM_FLAG = ""
+}
+
+mkdir build
+cd build
+
+c:\projects\TapTools\cmake-3.4.1-win32-x86\bin\cmake.exe -G $env:CMAKE_GENERATOR $env:CUSTOM_FLAG  -DCMAKE_BUILD_TYPE=Release .. > c:\projects\TapTools\configure.log
+c:\projects\TapTools\cmake-3.4.1-win32-x86\bin\cmake.exe --build . --config Release > c:\projects\TapTools\build.log
